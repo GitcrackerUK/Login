bd1996b44735a0d20c816c23fcc24d4c1f26d099
gitcrackerUK <pfsiwek@gmail.com>
2020-11-11 21:16:18 +0000

    initial

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Button from './components/common/button'
+
+
+function App(props) {
+  return (
+    <div>
+      <h1>Hello</h1>
+      <Button>Hello</Button>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/common/button.js
@@ -0,0 +1,17 @@
+import styled from "styled-components"
+
+const Button = styled.button`
+color:white;
+background:black;
+font-weight:bold;
+padding:8px;
+border-radius:4px;
+box-shadow:none;
+font-size:1em;
+border:none;
+width:100%;
+display:block;
+white-space:none;
+`
+
+export default Button
