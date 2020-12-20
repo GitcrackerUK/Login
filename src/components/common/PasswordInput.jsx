5c0e4f161059b81b75982e3d21915b8341119de2
gitcrackerUK <pfsiwek@gmail.com>
2020-12-20 19:18:11 +0000

    ternery with show and hide components for password

--- src/components/common/PasswordInput.jsx
+++ src/components/common/PasswordInput.jsx
@@ -5,15 +5,20 @@ import Input from 'components/common/form'
 
 const PasswordInputWrapper = styled.div`
 display:flex;
-~div{
-    margin-bottom:8px;
-}
 `
 
 const StPasswordInput = styled(Input).attrs(() => ({
     type: "password",
     placeholder: "Password"
 }))`
+border-top-right-radius:0px;
+border-bottom-right-radius:0px;
+`
+const StPasswordInputShow = styled(Input).attrs(() => ({
+    type: "text",
+    placeholder: "Password"
+}))`
+
 border-top-right-radius:0px;
 border-bottom-right-radius:0px;
 `
@@ -34,25 +39,17 @@ user-select:none;
 color:black;
 `
 
-
-
 export default function PasswordInput(props) {
     const [showPassword, setShowPassword] = useState(false)
 
     return (
         <>
             <PasswordInputWrapper>
-                <StPasswordInput {...props} />
-               
+               {showPassword?  <StPasswordInputShow {...props}/> : <StPasswordInput {...props} /> } 
             <ToggledButton onClick={()=>setShowPassword((s)=>!s)}>
                { showPassword?'Hide':'Show'}
             </ToggledButton>
             </PasswordInputWrapper>
-          <div>
-              {
-                  showPassword?props.value:''
-              }
-          </div>
         </>
     )
 } 
