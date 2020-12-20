22249e221e8fda344b198c214d1a821a0d81767e
gitcrackerUK <pfsiwek@gmail.com>
2020-12-20 17:48:08 +0000

    added two form fields to login component

--- src/components/pages/login.jsx
+++ src/components/pages/login.jsx
@@ -1,5 +1,6 @@
-import React from 'react'
+import React, {useState}from 'react'
 import Layout from 'components/common/layout'
+import Input from 'components/common/form'
 import styled from 'styled-components'
 
 const Form = styled.form`
@@ -14,14 +15,38 @@ border-radius:4px;
 
 
 export default function Login() {
+    const [formField, setFormField] = useState({username:'',password:''});
+    function handleInputChange(e){
+        e.persist();
+        setFormField(
+            s=>({
+                ...s,
+                [e.target.name]:e.target.value
+            }
+            )
+        )
+    }
     return (
         <Layout>
 
             <div>
                 <h1>Login</h1>
-              <Form>
-                  
-              </Form>
+                <Form>
+                    <Input 
+                    value={formField.username}
+                    onChange={handleInputChange}
+                    type="text"
+                    name="username" 
+                    placeholder="Username" />
+                </Form>
+                <Form>
+                    <Input 
+                    value={formField.password}
+                    onChange={handleInputChange}
+                    type="password"
+                    name="password" 
+                    placeholder="Password" />
+                </Form>
             </div>
 
         </Layout>
