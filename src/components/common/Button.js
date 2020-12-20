d645c7d5dbea4e6c7d363ef37b7688b9ed8185da
gitcrackerUK <pfsiwek@gmail.com>
2020-12-20 20:25:31 +0000

    Button with Pink and Large atributes

---
+++ src/components/common/Button.js
@@ -0,0 +1,23 @@
+import styled,{css} from 'styled-components'
+
+const Button = styled.button`
+${
+    p=>p.Large?css`
+    font-size:1.1em;
+    height:50px;
+    `:css`
+    font-size:1em;
+    height:40px;
+    `
+}
+color:#fff;
+font-weight:bold;
+padding:4px 8px;
+border: 1px solid #ccc;
+border-radius:4px;
+width:100%;
+box-sizing:border-box;
+background-color:${p=>p.Pink?"#f8049c":"#fdd54f"};
+`
+
+export default Button
