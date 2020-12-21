aed81183eb6d83a35f7b819a3199748c3810472b
gitcrackerUK <pfsiwek@gmail.com>
2020-12-21 23:20:26 +0000

    Light/Dark Themes

---
+++ src/Themes/dark.js
@@ -0,0 +1,9 @@
+const theme = {
+    id:"dark",
+    PrimaryColor:'black',
+    SecondaryColor:'#191970',
+    BodyColor:"black",
+    TextColor:"white"
+    }
+    
+    export default theme
---
+++ src/Themes/light.js
@@ -0,0 +1,8 @@
+const theme = {
+    id:"light",
+    PrimaryColor:'#f8049c',
+    SecondaryColor:'#fdd54f',
+    BodyColor:"white",
+    TextColor:"black"
+    }
+ export default theme;
