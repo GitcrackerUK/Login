dd3debfb96885178339f6c578dcb73062bbb8f41
gitcrackerUK <pfsiwek@gmail.com>
2020-12-21 10:29:58 +0000

    Toggle component for navbar to change themes

---
+++ src/components/common/Toggler.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const TogglerWrapper = styled.div`
+width:50px;
+min-width:50px;
+height:25px;
+border-radius:25px;
+border:1px solid #666;
+margin:auto;
+display:flex;
+background:black;
+`
+function Toggler(){
+    return(
+        <TogglerWrapper>
+
+
+
+        </TogglerWrapper>
+    )
+}
+
+
+export default Toggler
